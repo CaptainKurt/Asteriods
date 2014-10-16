89d1472331cef90a634a17ed9e4c0c19e87bb653
CaptainKurt <walkkg12@wfu.edu>
2014-10-16 11:17:19 +0200

    Initial Commit

---
+++ Asteriods/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Asteriods
+//
+//  Created by Kurt Walker on 10/16/14.
+//  Copyright (c) 2014 AIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
